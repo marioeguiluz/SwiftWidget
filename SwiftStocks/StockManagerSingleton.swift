92845ecf7f7a26a878f42e6c96e500f89ebf466c
MARIO EGUILUZ ALEBICTO <mario@MARIOs-MacBook-Pro.local>
2014-08-27 00:31:45 +0200

    Project first push

---
+++ SwiftStocks/StockManagerSingleton.swift
@@ -0,0 +1,71 @@
+//
+//  StockManagerSingleton.swift
+//  SwiftStocks
+//
+//  Created by MARIO EGUILUZ ALEBICTO on 18/08/14.
+//  Copyright (c) 2014 MARIO EGUILUZ ALEBICTO. All rights reserved.
+//
+
+import Foundation
+let kNotificationStocksUpdated = "stocksUpdated"
+
+class StockManagerSingleton {
+    
+    //Singleton Init
+    class var sharedInstance : StockManagerSingleton {
+    struct Static {
+        static let instance : StockManagerSingleton = StockManagerSingleton()
+        }
+        return Static.instance
+    }
+    
+    /*!
+    * @discussion Function that given an array of symbols, get their stock prizes from yahoo and send them inside a NSNotification UserInfo
+    * @param stocks An Array of tuples with the symbols in position 0 of each tuple
+    */
+    func updateListOfSymbols(stocks:Array<(String,Double)>) ->() {
+        
+        //1: YAHOO Finance API: Request for a list of symbols example:
+        //http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.quotes where symbol IN ("AAPL","GOOG","FB")&format=json&env=http://datatables.org/alltables.env
+       
+        //2: Build the URL as above with our array of symbols
+        var stringQuotes = "(";
+        for quoteTuple in stocks {
+            stringQuotes = stringQuotes+"\""+quoteTuple.0+"\","
+        }
+        stringQuotes = stringQuotes.substringToIndex(stringQuotes.endIndex.predecessor())
+        stringQuotes = stringQuotes + ")"
+
+        var urlString:String = ("http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.quotes where symbol IN "+stringQuotes+"&format=json&env=http://datatables.org/alltables.env").stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+        var url : NSURL = NSURL.URLWithString(urlString)
+        var request: NSURLRequest = NSURLRequest(URL:url)
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        
+        //3: Completion block/Clousure for the NSURLSessionDataTask
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            
+            if(error) {
+                println(error.localizedDescription)
+            }
+            else {
+                var err: NSError?
+                //4: JSON process
+                var jsonDict = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+                if(err != nil) {
+                    println("JSON Error \(err!.localizedDescription)")
+                }
+                else {
+                    //5: Extract the Quotes and Values and send them inside a NSNotification
+                    var quotes:NSArray = jsonDict.objectForKey("query").objectForKey("results").objectForKey("quote") as NSArray
+                    dispatch_async(dispatch_get_main_queue(), {
+                        NSNotificationCenter.defaultCenter().postNotificationName(kNotificationStocksUpdated, object: nil, userInfo: [kNotificationStocksUpdated:quotes])
+                        })
+                }
+            }
+        })
+        
+        //6: DONT FORGET to LAUNCH the NSURLSessionDataTask!!!!!!
+        task.resume()
+    }
+}
---
+++ SwiftStocks/StocksTableViewController.swift
@@ -0,0 +1,105 @@
+//
+//  StocksTableViewController.swift
+//  SwiftStocks
+//
+//  Created by MARIO EGUILUZ ALEBICTO on 07/08/14.
+//  Copyright (c) 2014 MARIO EGUILUZ ALEBICTO. All rights reserved.
+//
+
+import UIKit
+
+class StocksTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //1
+    private var stocks: [(String,Double)] = [("AAPL",+1.5),("FB",+2.33),("GOOG",-4.3)]
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //2
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "stocksUpdated:", name: kNotificationStocksUpdated, object: nil)
+        self.updateStocks()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    //UITableViewDataSource
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int  {
+        return stocks.count
+    }
+
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        var cell:UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "cellId")
+        cell.textLabel.text = stocks[indexPath.row].0 //position 0 of the tuple: The Symbol "AAPL"
+        cell.detailTextLabel.text = "\(stocks[indexPath.row].1)" + "%" //position 1 of the tuple: The value "1.5" into String
+        return cell
+    }
+    
+    //UITableViewDelegate
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        
+    }
+    
+    //Customize the cell
+    func tableView(tableView: UITableView!, willDisplayCell cell: UITableViewCell!, forRowAtIndexPath indexPath: NSIndexPath!) {
+        switch stocks[indexPath.row].1 {
+            case let x where x < 0.0:
+                cell.backgroundColor = UIColor(red: 255.0/255.0, green: 59.0/255.0, blue: 48.0/255.0, alpha: 1.0)
+            case let x where x > 0.0:
+                cell.backgroundColor = UIColor(red: 76.0/255.0, green: 217.0/255.0, blue: 100.0/255.0, alpha: 1.0)
+            case let x:
+                cell.backgroundColor = UIColor(red: 44.0/255.0, green: 186.0/255.0, blue: 231.0/255.0, alpha: 1.0)
+        }
+        
+        cell.textLabel.textColor = UIColor.whiteColor()
+        cell.detailTextLabel.textColor = UIColor.whiteColor()
+        cell.textLabel.font = UIFont(name: "HelveticaNeue-CondensedBold", size: 48)
+        cell.detailTextLabel.font = UIFont(name: "HelveticaNeue-CondensedBold", size: 48)
+        cell.textLabel.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25)
+        cell.textLabel.shadowOffset = CGSize(width: 0, height: 1)
+        cell.detailTextLabel.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25)
+        cell.detailTextLabel.shadowOffset = CGSize(width: 0, height: 1)
+    }
+    
+    //Customize the height of the cell
+    func tableView(tableView: UITableView!, heightForRowAtIndexPath indexPath: NSIndexPath!) -> CGFloat {
+        return 120
+    }
+    
+    //Stock updates
+    //3
+    func updateStocks() {
+        let stockManager:StockManagerSingleton = StockManagerSingleton.sharedInstance
+        stockManager.updateListOfSymbols(stocks)
+        
+        //Repeat this method after 15 secs. (For simplicity of the tutorial we are not cancelling it never)
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(15 * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(),
+            {
+                self.updateStocks()
+            }
+        )
+    }
+    
+    //4
+    func stocksUpdated(notification:NSNotification) {
+        let stocksReceived:NSArray = notification.userInfo[kNotificationStocksUpdated] as NSArray
+        stocks.removeAll(keepCapacity: false)
+        for quote in stocksReceived {
+            let quoteDict:NSDictionary = quote as NSDictionary
+            var changeInPercentString = quoteDict["ChangeinPercent"] as String
+            let changeInPercentStringClean: NSString = (changeInPercentString as NSString).substringToIndex(countElements(changeInPercentString)-1)
+            stocks.append(quoteDict["symbol"] as String,changeInPercentStringClean.doubleValue)
+        }
+        tableView.reloadData()
+        NSLog("Symbols Values updated :)")
+    }
+}
+
